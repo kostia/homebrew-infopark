35b141424e5f2d9d9fee6be64ca2918921a33bc9
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2013-12-06 18:13:50 +0100

    [kick-it] Updated to version 0.0.4

--- kick-it.rb
+++ kick-it.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class KickIt < Formula
   homepage 'https://github.com/kostia/kick-it'
-  version '0.0.3'
+  version '0.0.4'
   url 'https://github.com/kostia/kick-it/archive/master.zip'
-  sha1 'b87c314f6453d55ec89f712ad7372b68de147603'
+  sha1 '178d3a04f516349fc8f174904630bd9247ecc078'
 
   def install
     bin.install 'bin/kick-it'
