32c18905272c2f1bfcd20249776b4a4183a5b795
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2013-12-12 20:30:29 +0100

    [kick-it] Added caveats
    Fixes #1

--- kick-it.rb
+++ kick-it.rb
@@ -9,4 +9,11 @@ class KickIt < Formula
   def install
     bin.install 'bin/kick-it'
   end
+
+  def caveats; <<-EOS.undent
+    Type "kick-it -h" for a short description.
+    Put configuration in ~/.config/kick-it/config.yml (will be generated on the first run).
+    For further documentation refer to https://github.com/kostia/kick-it.
+    EOS
+  end
 end
