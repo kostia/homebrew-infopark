9fc03321f5bd51011aaf6b14fa4870f43ca74287
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2014-05-30 13:16:04 +0200

    [kick-it] 0.4.0

--- kick-it.rb
+++ kick-it.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class KickIt < Formula
   homepage 'https://github.com/infopark/kick-it'
-  version '0.3.0'
-  url 'https://github.com/infopark/kick-it/archive/0.3.0.zip'
-  sha1 '17819fc8c2d2c4c6acd8757add79e84b1265db74'
+  version '0.4.0'
+  url 'https://github.com/infopark/kick-it/archive/0.4.0.zip'
+  sha1 '2f912631cba8fccaebda99c4fccad57a2a1ac705'
 
   def install
     bin.install 'bin/kick-it'
